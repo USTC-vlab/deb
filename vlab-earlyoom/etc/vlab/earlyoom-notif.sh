9b6c27616bd3ac751905c9826534acbb9a7f8b29
taoky <me@taoky.moe>
2022-12-02 20:41:14 +0800

    vlab-earlyoom init

---
+++ vlab-earlyoom/etc/vlab/earlyoom-notif.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+dbus-update-activation-environment DISPLAY=:0
+wall "由于内存不足，earlyoom 已杀死一些进程以保证用户界面流畅。如果多次看到此消息，请联系管理员添加内存。"
+sudo -u ubuntu DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus" notify-send "由于内存不足，earlyoom 已杀死一些进程以保证用户界面流畅。如果多次看到此消息，请联系管理员添加内存。"
