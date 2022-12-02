bdb6bb2774e209e4ee7f889bef6a021b41e72fe1
iBug <git@ibugone.com>
2022-12-03 01:08:19 +0800

    earlyoom: Put message in variable for reusing

--- vlab-earlyoom/etc/vlab/earlyoom-notif.sh
+++ vlab-earlyoom/etc/vlab/earlyoom-notif.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+MSG="由于内存不足，earlyoom 已结束一些进程以保证用户界面流畅。如果多次看到此消息，请联系管理员添加内存。"
+
+wall "$MSG"
 timeout -k 2 5 sudo -u ubuntu DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus" dbus-update-activation-environment DISPLAY=:0
-wall "由于内存不足，earlyoom 已杀死一些进程以保证用户界面流畅。如果多次看到此消息，请联系管理员添加内存。"
-timeout -k 2 5 sudo -u ubuntu DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus" notify-send "由于内存不足，earlyoom 已杀死一些进程以保证用户界面流畅。如果多次看到此消息，请联系管理员添加内存。"
+timeout -k 2 5 sudo -u ubuntu DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus" notify-send "$MSG"
