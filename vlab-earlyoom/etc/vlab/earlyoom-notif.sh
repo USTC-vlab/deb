4075876cfdfe0d0654591fe66b9f2f4a7908d781
taoky <me@taoky.moe>
2022-12-02 21:27:33 +0800

    Add timeout for notify-send

--- vlab-earlyoom/etc/vlab/earlyoom-notif.sh
+++ vlab-earlyoom/etc/vlab/earlyoom-notif.sh
@@ -2,4 +2,4 @@
 
 dbus-update-activation-environment DISPLAY=:0
 wall "由于内存不足，earlyoom 已杀死一些进程以保证用户界面流畅。如果多次看到此消息，请联系管理员添加内存。"
-sudo -u ubuntu DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus" notify-send "由于内存不足，earlyoom 已杀死一些进程以保证用户界面流畅。如果多次看到此消息，请联系管理员添加内存。"
+timeout -k 2 sudo -u ubuntu DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus" notify-send "由于内存不足，earlyoom 已杀死一些进程以保证用户界面流畅。如果多次看到此消息，请联系管理员添加内存。"
